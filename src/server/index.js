717887985bebde8ca1d35860a41506f573d1579d
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-05-02 12:48:00 +0200

    Setup google OAuth strategy

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,22 @@ require('dotenv').config()
 
 const port = 8000;
 const app = express();
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile.displayName);
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('json spaces', 2);
 
@@ -13,6 +29,16 @@ app.get('/', (req, res) => {
     }
   )
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('req', req)
+    res.redirect('http://localhost:3000');
+  });
 
 app.listen(port);
 console.log(`Serving at http://localhost:${port}`);

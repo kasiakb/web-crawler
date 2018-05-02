ef4dc5c984faed3537d2295651fbbd9d12b10d1c
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-05-02 10:37:44 +0200

    Create sign in button

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LoginPage from './LoginPage';
+
+class App extends Component {
+  render() {
+    return (
+      <LoginPage/>
+    );
+  }
+}
+
+export default App;

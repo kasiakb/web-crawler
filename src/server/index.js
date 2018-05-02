e9a827ad6f2a79482c99085602a81bd3117265e9
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-05-02 10:28:00 +0200

    Create basic express server

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const port = 8000;
+const app = express();
+
+app.set('json spaces', 2);
+
+app.get('/', (req, res) => {
+  res.json(
+    {
+      data: 'Here is JSON reply',
+    }
+  )
+})
+
+app.listen(port);
+console.log(`Serving at http://localhost:${port}`);

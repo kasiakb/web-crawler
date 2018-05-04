451d63dc54f7d6dcd1a4b4a568585b853a9d0d5a
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-05-04 17:02:39 +0200

    fetch siteAnalysis data from server

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import shortid from 'shortid';
+import moment from 'moment'
+import '../css/UserPanel.css'
 
 class UserPanel extends Component {
   constructor(props) {
@@ -11,6 +12,16 @@ class UserPanel extends Component {
     }
   }
 
+  componentWillMount() {
+    fetch('http://localhost:8000/siteAnalyses', {
+      method: 'GET',
+      mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(response => this.setState({siteAnalysis: response.data}))
+    .catch(error => console.error('Error:', error))
+  }
+
   titleChange(e) {
     this.setState({ titleInput: e.target.value });
   }
@@ -20,43 +31,69 @@ class UserPanel extends Component {
 
   addWebPage(e) {
     e.preventDefault();
-    const title = document.getElementById("title")
-    const url = document.getElementById("url")
-    const newWeb = [{
-      title: title.value,
-      url: url.value,
-      status: "pending"
-    }];
+    const newWeb = {
+      title: this.state.titleInput,
+      url: this.state.urlInput,
+    };
+    fetch('http://localhost:8000/siteAnalyses', {
+      body: JSON.stringify(newWeb),
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'user-agent': 'Mozilla/4.0 MDN Example',
+        'content-type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => this.setState({siteAnalysis: this.state.siteAnalysis.concat(response.data)}))
+    .catch(error => console.error('Error:', error))
     this.setState({
-      webs: this.state.webs.concat(newWeb),
       titleInput: '',
       urlInput: ''
     });
   }
 
   renderWebs() {
-    return this.state.webs.map((web) => {
-      return (
-        <li key={web.url}>
-          <p>{web.title}</p>
-          <p>{web.url}</p>
-          <p>{web.status}</p>
-          <button>Delate</button>
-        </li>
-      );
-    });
+    if(typeof this.state.siteAnalysis === 'undefined' || this.state.siteAnalysis === null) {
+      return null
+    }else{
+      
+      return this.state.siteAnalysis.map((web) => {
+        return (
+            <tr key={web.id}>
+              <td>{moment(web.attributes.updatedAt).format('lll')}</td>
+              <td>{web.attributes.title}</td>
+              <td>{web.attributes.url}</td>
+              <td>{web.attributes.status}</td>
+              <td><button>Delate</button></td>
+            </tr>
+        );
+      })
+    }
   }
 
   render() {
-    console.log(this.state)
     return (
-      <form>
-        <h1>Choose web page to crawl</h1>
-        <input type = "text" id="title" placeholder="web page title" value={this.state.titleInput} onChange={(e) => this.titleChange(e)}/>
-        <input type = "text" id="url" placeholder="web page url" value={this.state.urlInput} onChange={(e) => this.urlChange(e)}/>
-        <button onClick={(e)=>this.addWebPage(e)}>Analyse</button>
-        <ul className="webs-list">{ this.renderWebs() }</ul>
-      </form>
+      <div>
+        <form>
+          <h1>Choose web page to crawl</h1>
+          <input type = "text" id="title" placeholder="web page title" value={this.state.titleInput} onChange={(e) => this.titleChange(e)}/>
+          <input type = "text" id="url" placeholder="web page url" value={this.state.urlInput} onChange={(e) => this.urlChange(e)}/>
+          <button onClick={(e)=>this.addWebPage(e)}>Analyse</button>
+          <table>
+            <tbody> 
+              <tr>
+                <th>Data</th>
+                <th>Web Title</th>
+                <th>Web URL</th>
+                <th>Analysis Status</th>
+                <th>Delate</th>
+              </tr>
+              {this.renderWebs()}
+            </tbody>
+          </table>
+        </form>
+      </div>
     );
   }
 }

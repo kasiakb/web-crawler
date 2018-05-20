e9f58d92ae03a1e2bd1314d1589ee7009e577f09
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-05-20 16:47:53 +0200

    Create sign in and register validation

--- src/components/RegisterInputs.js
+++ src/components/RegisterInputs.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { Form} from 'react-final-form';
 import {
   onSubmit,
-  signValidation
+  registerValidation
 } from './InputValidationConst'
 import ButtonItem from './ButtonItem';
 import IntputItem from './IntputItem';
 
-class SignInInputs extends Component {
+class RegisterInputs extends Component {
 
   onSubmit = () => {
     return new Promise(resolve => {
@@ -18,27 +18,33 @@ class SignInInputs extends Component {
   myForm() {
     return (
     <div>
-      <h1>Sign in</h1>
+      <h1>Create a new account</h1>
       <Form
         onSubmit={onSubmit}
-        validate={signValidation}
+        validate={registerValidation}
         render={({ handleSubmit, reset, submitting, pristine, values }) => (
           <form
             onSubmit={handleSubmit}
-          >
+            >
             <IntputItem
               name={"email"}
-              type={"text"}
+              type={"email"}
               placeholder={"E-mail"}
             />
              <IntputItem
               name={"password"}
               type={"password"}
               placeholder={"Password"}
+              condition={"Must contain at least one number and one uppercase and lowercase letter and, at least 8 or more characters"}
+            />
+             <IntputItem
+              name={"confirm"}
+              type={"password"}
+              placeholder={"Confirm password"}
             />
             <div className="buttons">
               <ButtonItem
-                text={"Sign in"}
+                text={"Create"}
               />
             </div>
           </form>
@@ -46,7 +52,8 @@ class SignInInputs extends Component {
       }
       />
     </div>
-    )
+  )
+ 
   }
 
   render() {
@@ -58,4 +65,4 @@ class SignInInputs extends Component {
   }
 }
 
-export default SignInInputs
+export default RegisterInputs

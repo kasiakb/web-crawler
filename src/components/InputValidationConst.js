263200032ef40cee3ef3734dcd36da10637752a7
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-05-31 16:19:52 +0200

    Create possibility of changing the view after log in

--- src/components/InputValidationConst.js
+++ src/components/InputValidationConst.js
@@ -19,7 +19,6 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export const onSubmit = async values => {
   await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
 };
 
 const properEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
